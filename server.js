1dbe7f648382c700fe8cdf6ec6302f8608e522df
acorn617 <audentia617@gmail.com>
2023-11-14 22:59:37 +0900

    routing

--- server.js
+++ server.js
@@ -2,12 +2,28 @@ const express = require('express')
 const app = express()
 // This Express app starts a server( which creates the scaffolding for a full app with numerous JavaScript files, Jade templates, and sub-directories for various purposes.)
 
+
+app.use(express.static(__dirname + '/public'));
+// Express looks up the files in the order in which you set the static directories with the 'express.static' middleware function.
+
 app.listen(8080, () => {
     console.log(' working on http://localhost:8080 ')
 })
 // Bind and listen to the connections on the specified host and port.
 
 app.get('/', (req , res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname+'/index.html')
 }) 
-// The app responds with “Hello World!” for requests to the root URL (/) .
+// The app responds with showing 'index.html' file for requests to the root URL (/) .
+
+
+app.get('/news', (req , res) => {
+  res.send('Raining!')
+}) 
+// The app responds with “Raining!” for requests to the URL (/news) .
+
+app.get('/shop', (req , res) => {
+  res.send('shop page!')
+}) 
+// The app responds with “shop page!” for requests to the URL (/shop) .
+
