08fa266689180ce710c004c03c0d43622fb694b1
acorn617 <audentia617@gmail.com>
2023-11-15 21:23:14 +0900

    take out data from db (1)

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express()
 app.use(express.static(__dirname + '/public'));
 // Express looks up the files in the order in which you set the static directories with the 'express.static' middleware function.
 
+const { MongoClient } = require('mongodb')
+
+
+let db
+const url = 'mongodb+srv://admin:Pikachu1@cluster0.u8fx7lz.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('success for connecting DB')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
+})
+
+// 9-18: Connect db to Application
+
 app.listen(8080, () => {
     console.log(' working on http://localhost:8080 ')
 })
@@ -14,16 +28,22 @@ app.listen(8080, () => {
 app.get('/', (req , res) => {
   res.sendFile(__dirname+'/index.html')
 }) 
-// The app responds with showing 'index.html' file for requests to the root URL (/) .
+// The app responds with showing 'index.html' file for requests to the root URL (/).
 
 
 app.get('/news', (req , res) => {
   res.send('Raining!')
 }) 
-// The app responds with “Raining!” for requests to the URL (/news) .
+// The app responds with “Raining!” for requests to the URL (/news).
 
 app.get('/shop', (req , res) => {
   res.send('shop page!')
 }) 
-// The app responds with “shop page!” for requests to the URL (/shop) .
+// The app responds with “shop page!” for requests to the URL (/shop).
 
+app.get('/list', async (req , res) => {
+  let result = await db.collection('post').find().toArray()
+  console.log(result[0].title)
+})
+
+// Take out the data from db and show it on the list page(/list).
